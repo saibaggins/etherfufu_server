e3532d9aecf76f299740edb2d1a1871db83b4d9b
Karthik Muralidharan <kmuralidharan@groupon.com>
2017-02-14 00:01:00 +0530

    moved middlewares to separate file

--- boot.go
+++ boot.go
@@ -8,30 +8,13 @@ import (
 	"net/http"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		if c.Request.Method == "OPTIONS" {
-			fmt.Println("OPTIONS")
-			c.AbortWithStatus(http.StatusOK)
-		} else {
-			c.Next()
-		}
-	}
-}
-
 func main() {
 	utils.StartDBSession("mongodb://localhost:27017/test")
 	session := utils.GetDB()
 	defer session.Close()
 
-	fmt.Println(session.DB("test").C("sessions").Count())
+	result, _ := session.DB("test").C("sessions").Count()
+	fmt.Println(result)
 
 	r := gin.Default()
 
