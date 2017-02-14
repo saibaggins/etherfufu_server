c34b240f69bd808325e2eb6f1256049503ac25cd
Karthik Muralidharan <kmuralidharan@groupon.com>
2017-02-14 22:42:16 +0530

    Add Vendoring dependencies

--- models/selection_option.go
+++ models/selection_option.go
@@ -4,6 +4,7 @@ type SelectionOption struct {
 	Attribute string   `json:"attribute"`
 	Enum      []string `json:"enum,omitempty"`
 	Required  bool     `json:"required"`
+	Type      string   `json:"type"`
 	Metric    string   `json:"metric,omitempty"`
 }
 
@@ -11,9 +12,12 @@ type SelectionOptionModel struct{}
 
 func (sel *SelectionOptionModel) FindAll() []SelectionOption {
 	return []SelectionOption{
-		{Attribute: "Accent", Enum: []string{"English", "Indian", "American", "Hispanic", "Chinese"}, Required: true},
-		{Attribute: "Recording Type", Enum: []string{"mic", "headphone"}, Required: true},
-		{Attribute: "Height", Metric: "cm", Required: true},
-		{Attribute: "Audio Sample Frequency", Metric: "hz", Required: true},
+		{Attribute: "Gender", Enum: []string{"male", "female"}, Required: true, Type: "String"},
+		{Attribute: "Ambience", Enum: []string{"Conf Room", "Office", "Car", "Street"}, Required: true, Type: "String"},
+		{Attribute: "Accent", Enum: []string{"English", "Indian", "American", "Hispanic", "Chinese"}, Required: true, Type: "String"},
+		{Attribute: "Speech Input", Enum: []string{"Headphone", "Phone Mic"}, Required: true, Type: "String"},
+		{Attribute: "Distance", Metric: "cm", Required: true, Type: "Integer"},
+		{Attribute: "Audio Sampling Rate", Metric: "hz", Required: true, Type: "Integer"},
+		{Attribute: "Speech Type", Enum: []string{"Command", "Sentence", "Paragraph"}, Required: true, Type: "String"},
 	}
 }
